efd6cc30938210dce96d24ff5d6230d862f31c2a
ma8sa <mahtabsandhu@research.iiit.ac.in>
2022-11-28 15:04:58 -0500

    adding odom

--- packages/avoiders/src/avoid_obstacle.py
+++ packages/avoiders/src/avoid_obstacle.py
@@ -39,6 +39,14 @@ class Avoider(DTROS):
             "~car_cmd", Twist2DStamped, queue_size=1, dt_topic_type=TopicType.CONTROL
         )
 
+        self.timestamp = None
+        self.x = 0.0
+        self.y = 0.0
+        self.z = 0.0
+        self.yaw = 0.0
+        self.q = [0.0, 0.0, 0.0, 1.0]
+        self.tv = 0.0
+        self.rv = 0.0
         #ROS
         #TODO : 1) get the subscirbers working 
         #TODO : 1.1) Test this using fake data
@@ -55,9 +63,89 @@ class Avoider(DTROS):
     
         #ts = message_filters.TimeSynchronizer([image_sub, info_sub], 10)
         #ts.registerCallback(self.callback)
+        
+        # subscirber
+        self.sub_encoder_left = message_filters.Subscriber("~left_wheel", WheelEncoderStamped)
+        self.sub_encoder_right = message_filters.Subscriber("~right_wheel", WheelEncoderStamped)
+
+        self.ts_encoders = message_filters.ApproximateTimeSynchronizer(
+            [self.sub_encoder_left, self.sub_encoder_right], 10, 0.5
+        )
 
         self.callback(0,0)
 
+
+    def cb_ts_encoders(self, left_encoder, right_encoder):
+        timestamp_now = rospy.get_time()
+
+        # Use the average of the two encoder times as the timestamp
+        left_encoder_timestamp = left_encoder.header.stamp.to_sec()
+        right_encoder_timestamp = right_encoder.header.stamp.to_sec()
+        timestamp = (left_encoder_timestamp + right_encoder_timestamp) / 2
+
+        if not self.left_encoder_last:
+            self.left_encoder_last = left_encoder
+            self.right_encoder_last = right_encoder
+            self.encoders_timestamp_last = timestamp
+            self.encoders_timestamp_last_local = timestamp_now
+            return
+
+        # Skip this message if the time synchronizer gave us an older message
+        dtl = left_encoder.header.stamp - self.left_encoder_last.header.stamp
+        dtr = right_encoder.header.stamp - self.right_encoder_last.header.stamp
+        if dtl.to_sec() < 0 or dtr.to_sec() < 0:
+            self.loginfo("Ignoring stale encoder message")
+            return
+
+        left_dticks = left_encoder.data - self.left_encoder_last.data
+        right_dticks = right_encoder.data - self.right_encoder_last.data
+
+        left_distance = left_dticks * 1.0 / self.ticks_per_meter
+        right_distance = right_dticks * 1.0 / self.ticks_per_meter
+
+        # Displacement in body-relative x-direction
+        distance = (left_distance + right_distance) / 2
+
+        # Change in heading
+        dyaw = (right_distance - left_distance) / self.wheelbase
+
+        dt = timestamp - self.encoders_timestamp_last
+
+        if dt < 1e-6:
+            self.logwarn("Time since last encoder message (%f) is too small. Ignoring" % dt)
+            return
+
+        self.tv = distance / dt
+        self.rv = dyaw / dt
+
+        if self.debug:
+            self.loginfo(
+                "Left wheel:\t Time = %.4f\t Ticks = %d\t Distance = %.4f m"
+                % (left_encoder.header.stamp.to_sec(), left_encoder.data, left_distance)
+            )
+
+            self.loginfo(
+                "Right wheel:\t Time = %.4f\t Ticks = %d\t Distance = %.4f m"
+                % (right_encoder.header.stamp.to_sec(), right_encoder.data, right_distance)
+            )
+
+            self.loginfo(
+                "TV = %.2f m/s\t RV = %.2f deg/s\t DT = %.4f" % (self.tv, self.rv * 180 / math.pi, dt)
+            )
+
+        dist = self.tv * dt
+        dyaw = self.rv * dt
+
+        self.yaw = self.angle_clamp(self.yaw + dyaw)
+        self.x = self.x + dist * math.cos(self.yaw)
+        self.y = self.y + dist * math.sin(self.yaw)
+        self.q = tr.quaternion_from_euler(0, 0, self.yaw)
+        self.timestamp = timestamp
+
+        self.left_encoder_last = left_encoder
+        self.right_encoder_last = right_encoder
+        self.encoders_timestamp_last = timestamp
+        self.encoders_timestamp_last_local = timestamp_now
 
     def callback(self,obstacles,lane):
 
@@ -76,6 +164,7 @@ class Avoider(DTROS):
 
             iter_ = 0
 
+            self.ts_encoders.registerCallback(self.cb_ts_encoders)
             while(self.state == 0):
 
                 car_control_msg = Twist2DStamped()
@@ -87,7 +176,7 @@ class Avoider(DTROS):
                 car_control_msg.omega = message_count[ iter_ % m_len ]
             #self.pub_motor.pub()
                 self.pub_motor.publish(car_control_msg)
-                
+                print(self.x,self.y,self.z)
                 cur_time = rospy.Time.now()
                 if cur_time.secs - start_time.secs > 1:
                     start_time = rospy.Time.now()
