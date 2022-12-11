d7421fcfd9f2efb8d380a96b470337aab51c076a
ma8sa <mahtabsandhu@research.iiit.ac.in>
2022-12-11 17:20:36 -0500

    fusing

--- packages/avoiders/src/avoid_obstacle.py
+++ packages/avoiders/src/avoid_obstacle.py
@@ -97,7 +97,7 @@ class Avoider(DTROS): #comment here
             
 
         self.sub_path = rospy.Publisher(
-            "~avoidance_path", Polygon, self.execute,queue_size=10)
+            "/agent/detect_obstacle/avoidance_path", Polygon, self.execute,queue_size=10)
         #if path is created then only check for this 
         # IF 
         #self.callback(0,0)
