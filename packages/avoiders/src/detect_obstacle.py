e3b533513daf67da4a4963c87102dd6acb9c5c76
ma8sa <mahtabsandhu@research.iiit.ac.in>
2022-12-12 14:09:05 -0500

    bot runs

--- packages/avoiders/src/detect_obstacle.py
+++ packages/avoiders/src/detect_obstacle.py
@@ -130,7 +130,7 @@ class Avoider(DTROS): #comment here
         lane_info[0] = lane_info[0] + (self.LANE_WIDTH/2)
         
         obstacle_info = self.rotate((0,0), np.array(list(obstacle_info)), lane_info[1])
-        obstacle_info = mp.array([0,40]) #TODO remove this 
+        obstacle_info = np.array([0,40]) #TODO remove this 
     
         # Assume obstacle_info[1] > 20
         safe_distance = 10
