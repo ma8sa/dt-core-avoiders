c70ebd68210aac5e1191b3f1cf41cd0dbc85b39e
ma8sa <mahtabsandhu@research.iiit.ac.in>
2022-12-12 16:43:18 -0500

    testing on duckonaut

--- packages/avoiders/src/avoid_obstacle.py
+++ packages/avoiders/src/avoid_obstacle.py
@@ -90,8 +90,11 @@ class Avoider(DTROS): #comment here
         #self.sub_encoder_left = message_filters.Subscriber("/agent/left_wheel_encoder_node/tick", WheelEncoderStamped)
         #self.sub_encoder_right = message_filters.Subscriber("/agent/right_wheel_encoder_node/tick", WheelEncoderStamped)
 
-        self.sub_encoder_left = message_filters.Subscriber("/calibratedduck/left_wheel_encoder_node/tick", WheelEncoderStamped)
-        self.sub_encoder_right = message_filters.Subscriber("/calibratedduck/right_wheel_encoder_node/tick", WheelEncoderStamped)
+        #self.sub_encoder_left = message_filters.Subscriber("/calibratedduck/left_wheel_encoder_node/tick", WheelEncoderStamped)
+        #self.sub_encoder_right = message_filters.Subscriber("/calibratedduck/right_wheel_encoder_node/tick", WheelEncoderStamped)
+
+        self.sub_encoder_left = message_filters.Subscriber("/duckonaut/left_wheel_encoder_node/tick", WheelEncoderStamped)
+        self.sub_encoder_right = message_filters.Subscriber("/duckonaut/right_wheel_encoder_node/tick", WheelEncoderStamped)
 
         self.ts_encoders = message_filters.ApproximateTimeSynchronizer(
             [self.sub_encoder_left, self.sub_encoder_right], 1, 1
@@ -101,8 +104,10 @@ class Avoider(DTROS): #comment here
 
         #self.sub_path = rospy.Subscriber(
         #    "/agent/detect_obstacle/avoidance_path", Polygon, self.execute,queue_size=10)
+        #self.sub_path = rospy.Subscriber(
+        #    "/calibratedduck/detect_obstacle/avoidance_path", Polygon, self.execute,queue_size=10)
         self.sub_path = rospy.Subscriber(
-            "/calibratedduck/detect_obstacle/avoidance_path", Polygon, self.execute,queue_size=10)
+            "/duckonaut/detect_obstacle/avoidance_path", Polygon, self.execute,queue_size=10)
         #if path is created then only check for this 
         # IF 
         #self.callback(0,0)
