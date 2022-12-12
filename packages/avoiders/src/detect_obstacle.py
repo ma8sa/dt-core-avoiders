1138b40bdae56de122a59a25b6deeaded43812b9
ma8sa <mahtabsandhu@research.iiit.ac.in>
2022-12-12 18:07:35 -0500

    testing on stark,non hardcoded

--- packages/avoiders/src/detect_obstacle.py
+++ packages/avoiders/src/detect_obstacle.py
@@ -133,7 +133,7 @@ class Avoider(DTROS): #comment here
         
         obstacle_info = self.rotate((0,0), np.array(list(obstacle_info)), lane_info[1])
 
-        obstacle_info = np.array([30,40]) #TODO remove this 
+        #obstacle_info = np.array([30,40]) #TODO remove this 
     
         # Assume obstacle_info[1] > 20
         safe_distance = 15
@@ -185,7 +185,7 @@ class Avoider(DTROS): #comment here
         
         lane_info[0] = lane_info[0] + (self.LANE_WIDTH/2)
         obstacle_info = self.rotate((0,0), np.array(list(obstacle_info)), lane_info[1])
-        obstacle_info = np.array([30,40]) #TODO remove this 
+        #obstacle_info = np.array([30,40]) #TODO remove this 
         print("updated obstacle ",obstacle_info)    
         obstacle_2_lane_x = obstacle_info[0] + lane_info[0]
         to_avoid = np.logical_not(np.logical_or(obstacle_2_lane_x < -self.OBSTACLE_RADIUS, 
